f93b6a02e13aab443bb98cdd9be9dada83a8bbcf
Edison Chan <edisonchanwk@gmail.com>
2022-07-13 23:21:39 +0800

    chore: add commitlint and shareable commitizen script package

---
+++ packages/custom-commit/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
+
+bootstrap({
+  cliPath: path.join(__dirname, '../../node_modules/commitizen'),
+  config: {
+    path: 'cz-conventional-changelog'
+  }
+});
